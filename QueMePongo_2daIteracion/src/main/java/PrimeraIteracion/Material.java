a48226b9d7294c7de1e7fc70b2baf4344e358367
GRodriguezPardo <gonrpardo@hotmail.com>
2021-04-27 02:28:03 -0300

    Agrego compatibilidad de tipos para cada material

--- QueMePongo_2daIteracion/src/main/java/PrimeraIteracion/Material.java
+++ QueMePongo_2daIteracion/src/main/java/PrimeraIteracion/Material.java
@@ -1,5 +1,24 @@
 package primeraIteracion;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum Material {
-    LINO, LANA, SEDA
+    CUERO(TipoPrenda.ZAPATO,TipoPrenda.PANTALON),
+    LANA(TipoPrenda.PANTALON,TipoPrenda.REMERA),
+    JEAN(TipoPrenda.PANTALON,TipoPrenda.GORRA),
+    SEDA(TipoPrenda.CAMISA),
+    ALGODON(TipoPrenda.PANTALON,TipoPrenda.REMERA,TipoPrenda.CAMISA),
+    ACETATO(TipoPrenda.PANTALON),
+    ;
+
+    List<TipoPrenda> tiposValidos;
+
+    Material(TipoPrenda ... tipos){
+       this.tiposValidos = Arrays.asList(tipos);
+    }
+
+    public Boolean compatibleCon(TipoPrenda tipo){
+       return tiposValidos.contains(tipo);
+    }
 }
